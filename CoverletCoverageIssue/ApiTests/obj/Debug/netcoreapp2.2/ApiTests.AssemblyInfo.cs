94507f5f9f18dbab0ee93a2c25420271e66d870a
FATIH.KUCUK <fatih.kucuk@lcwaikiki.com>
2019-07-30 09:03:14 +0300

    gitignore added

--- CoverletCoverageIssue/ApiTests/obj/Debug/netcoreapp2.2/ApiTests.AssemblyInfo.cs
+++ CoverletCoverageIssue/ApiTests/obj/Debug/netcoreapp2.2/ApiTests.AssemblyInfo.cs
@@ -11,8 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("CoverletIssueRaiser, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\fatih.kucuk\\Documents\\Visual Studio 2017\\Projects\\CoverletIssueRaiser\\Co" +
-    "verletIssueRaiser", "CoverletIssueRaiser.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("CoverletIssueRaiser, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\dev\\github\\shared\\CoverletCoverageIssue\\CoverletIssueRaiser", "CoverletIssueRaiser.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
